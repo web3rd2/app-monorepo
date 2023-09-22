f8e08ade20c79b830ba0b91fde4b8ed688c785ac
joe <joe@bitvenus.com>
2023-09-22 23:20:05 +0800

    fix: 🐛 web support

--- packages/web/e2e.config.js
+++ packages/web/e2e.config.js
@@ -5,20 +5,12 @@ module.exports = {
   logger: {
     level: process.env.CI ? 'debug' : undefined,
   },
-  testRunner: {
-    args: {
-      '$0': 'yarn dlx jest',
-      config: path.resolve(process.cwd(), 'e2e/jest.config.js'),
-      maxWorkers: process.env.CI ? 2 : undefined,
-      _: [path.resolve(process.cwd(), 'e2e')],
-    },
-  },
   chrome: {
     args: [], // ['--headless', '--disable-gpu','--load-extension=${pathToYourUnpackedExtension}`'];
     // binaryPath:"",// 指定浏览器路径。
   },
   configurations: {
-    index: 'http://localhost:3000/',
+    index: 'http://localhost:3000',
     browser: 'chrome',
   },
 };
